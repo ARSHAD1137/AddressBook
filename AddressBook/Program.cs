7390f53d09973c12f4d6809c42156e52cc3f99a4
Arshad <arshadpinjari666@gmail.com>
2021-05-10 17:12:17 +0530

    Added.uc1

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBook
 {
@@ -7,10 +8,42 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
-            Implement myobj = new Implement();
-            myobj.myMethod1();
-            myobj.myMethod2();
-            myobj.myMethod3();
+            
+             Contact myObj = new Contact();
+
+            Console.WriteLine("First Name:");
+            myObj.FirstName = Console.ReadLine();
+            Console.WriteLine(myObj.FirstName);
+
+            Console.WriteLine("Last Name:");
+            myObj.LastName = Console.ReadLine();
+            Console.WriteLine(myObj.LastName);
+
+            Console.WriteLine("Address:");
+            myObj.Address = Console.ReadLine();
+            Console.WriteLine(myObj.Address);
+
+            Console.WriteLine("City:");
+            myObj.City = Console.ReadLine();
+            Console.WriteLine(myObj.City);
+
+            Console.WriteLine("State:");
+            myObj.State = Console.ReadLine();
+            Console.WriteLine(myObj.State);
+
+            Console.WriteLine("Pincode:");
+            myObj.Pincode = Console.ReadLine();
+            Console.WriteLine(myObj.Pincode);
+
+            Console.WriteLine("Phone Number:");
+            myObj.PhoneNumber = Console.ReadLine();
+            Console.WriteLine(myObj.PhoneNumber);
+
+            Console.WriteLine("Email:");
+            myObj.Email = Console.ReadLine();
+            Console.WriteLine(myObj.Email);
         }
+
     }
-}
+    
+}    
