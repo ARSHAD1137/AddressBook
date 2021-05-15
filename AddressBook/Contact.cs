c561a1fe3e476684596c60bab5833b199352749a
Arshad <arshadpinjari666@gmail.com>
2021-05-15 18:57:11 +0530

    Added.NewContact

--- AddressBook/Contact.cs
+++ AddressBook/Contact.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    public class Contact
+    class Contact
     {
-      
+       
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
